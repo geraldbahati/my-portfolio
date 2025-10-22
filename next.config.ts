c53f7e19fa2795ae293c4e56178ed6c8890688ff
geraldbahati <journeytoharvard@gmail.com>
2025-10-22 13:05:50 +0300

    Revert "feat: integrate PostHog for enhanced analytics tracking"
    This reverts commit b01abdcffae5aa150be5d70722dca4f58666a8d8.

--- next.config.ts
+++ next.config.ts
@@ -4,29 +4,12 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "images.unsplash.com",
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
       },
     ],
     qualities: [75, 85, 90, 100],
   },
-
-  // PostHog rewrites
-  async rewrites() {
-    return [
-      {
-        source: "/ingest/static/:path*",
-        destination: "https://eu-assets.i.posthog.com/static/:path*",
-      },
-      {
-        source: "/ingest/:path*",
-        destination: "https://eu.i.posthog.com/:path*",
-      },
-    ];
-  },
-
-  // Required to support PostHog trailing slash API requests
-  skipTrailingSlashRedirect: true,
 };
 
 export default nextConfig;
